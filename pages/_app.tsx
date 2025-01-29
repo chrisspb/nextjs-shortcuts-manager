b435db0b5146d311a5d857e7bb120fd2f3577dc8
chrisspb <paghbirk.christian@gmail.com>
2025-01-29 21:35:01 +0100

    Update _app.tsx with Toaster configuration

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,16 +1,37 @@
 import { SessionProvider } from 'next-auth/react';
 import { Toaster } from 'react-hot-toast';
+import { appWithTranslation } from 'next-i18next';
 import type { AppProps } from 'next/app';
 import '../styles/globals.css';
 
-export default function App({
+function App({
   Component,
   pageProps: { session, ...pageProps },
 }: AppProps) {
   return (
     <SessionProvider session={session}>
       <Component {...pageProps} />
-      <Toaster position="top-right" />
+      <Toaster 
+        position="top-right"
+        toastOptions={{
+          success: {
+            duration: 3000,
+            style: {
+              background: '#10B981',
+              color: 'white',
+            },
+          },
+          error: {
+            duration: 4000,
+            style: {
+              background: '#EF4444',
+              color: 'white',
+            },
+          },
+        }}
+      />
     </SessionProvider>
   );
 }
+
+export default appWithTranslation(App);
