d27df7a3636e4f1b45e7ec5de73afd1733acaa8b
chrisspb <paghbirk.christian@gmail.com>
2025-02-01 13:22:39 +0100

    remove user  navbar

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -31,9 +31,6 @@ const Navbar = () => {
           <div className="flex items-center space-x-4">
             {session && (
               <div className="flex items-center space-x-4">
-                <div className="text-sm text-gray-600">
-                  {session.user.email}
-                </div>
                 <button
                   onClick={() => signOut()}
                   className="inline-flex items-center px-4 py-2 border border-transparent text-sm font-medium rounded-lg text-white bg-red-600 hover:bg-red-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-red-500 transition-colors duration-200"
